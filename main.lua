da4aaa63609f4cdf7df00da6ecce104be3c34f3a
Tim Caswell <tim@creationix.com>
2025-02-11 16:28:21 -0700

    Change address

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 local is_luvit = false
 local ok, uv = pcall(require, 'luv')
 if not ok then
-    is_luvit = true
     uv = require 'uv'
+    is_luvit = true
 end
 
 -- Multicast UDP example
--- listens on 224.0.0.1 (all machines on subnet) on port 31896
+-- listens on 239.255.0.1 (all machines on subnet) on port 31896
 
 -- You can run this on any node on your local network,
 -- each instance will receive UDPs sent to the multicast address
@@ -15,7 +15,7 @@ end
 -- for the same UDP broadcast on the same port.
 
 -- test this by doing this from any machine on the network
--- echo "this is a test" | nc -u -w0 224.0.0.1 31896
+-- echo "this is a test" | nc -u -w0 239.255.0.1 31896
 
 -- Create a TCP server so other nodes can connect directly to us and have reliable
 -- RPC conversations
@@ -34,14 +34,14 @@ assert(udp_sender:bind(address.ip, address.port, { reuseaddr = false }))
 local multi_address = {
     family = "inet",
     port = 31896,
-    ip = "224.0.0.1"
+    ip = "239.255.0.1"
 }
 -- Create a UDP socket for receiving multicast messages
 local multi = assert(uv.new_udp(multi_address.family))
--- Bind to 224.0.0.1:31896, allow port re-use so that multiple instances
+-- Bind to 239.255.0.1:31896, allow port re-use so that multiple instances
 -- of this program can all subscribe to the UDP broadcasts
 assert(multi:bind(multi_address.ip, multi_address.port, { reuseaddr = true }))
--- Join the multicast group on 224.0.0.1
+-- Join the multicast group on 239.255.0.1
 assert(multi:set_membership(multi_address.ip, "0.0.0.0", "join"))
 print(string.format("Multicast receiver listening on %s:%s\n",
     multi_address.ip, multi_address.port));
@@ -59,14 +59,14 @@ end)
 
 
 local timer = assert(uv.new_timer())
-timer:start(1000, 1000, function()
+timer:start(2000, 2000, function()
     print(string.format("sending from %s:%s", udp_sender:getsockname().ip, udp_sender:getsockname().port))
     assert(multi:send("coming from multi", multi_address.ip, multi_address.port, function(err)
         if err then
             print("send error: " .. tostring(err))
         end
     end))
-    assert(udp_sender:send(tostring(uv.hrtime()), multi_address.ip, multi_address.port, function(err)
+    assert(udp_sender:send("Tim: " .. tostring(uv.hrtime()), multi_address.ip, multi_address.port, function(err)
         if err then
             print("send error: " .. tostring(err))
         end
